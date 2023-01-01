45327cd3b0795325fc9768074934714e1465fc4d
Fatih20 <fnri39@protonmail.com>
2023-01-01 18:17:39 +0700

    Responsive project card.

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -11,6 +11,10 @@ module.exports = {
         horizontalPaddingDesktop: "1rem",
         horizontalPaddingMobile: "1.5rem",
       },
+      minHeight: {
+        cardDesktop: "12rem",
+        cardMobile: "16rem",
+      },
       boxShadow: {
         hoveredCard: "0 0 60px -15px rgba(0, 0, 0, 0.3)",
       },
