256de2714865d0a5d75ee6a257f7fdccdbf26022
Fatih20 <fnri39@protonmail.com>
2023-06-19 02:31:45 +0700

    Initial work on header rework.

---
+++ src/components/Header.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+  import type { HeaderOption } from "@/types/types";
+
+  //   type HeaderProp = {
+  //     activeNavigationOption: HeaderOption;
+  //   };
+
+  export let activeNavigationOption: HeaderOption;
+
+  let hovered = false;
+</script>
+
+<div class="fixed inset-0 w-screen h-screen bg-silver flex items-center">
+  <nav
+    class={`bg-black ${
+      hovered ? "translate-x-0" : "-translate-x-full"
+    } h-full p-8 relative transition-transform`}
+    on:mouseover={() => (hovered = true)}
+    on:mouseleave={() => (hovered = false)}
+    on:focus={() => {}}
+  >
+    <div class="absolute z-10 top-0 bottom-0 left-full h-full w-4" />
+  </nav>
+</div>
