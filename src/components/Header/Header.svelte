c32eed775dc1c44a22d6046678b031627586933a
Fatih20 <fnri39@protonmail.com>
2023-06-19 03:06:33 +0700

    Continued work on header.

--- src/components/Header/Header.svelte
+++ src/components/Header/Header.svelte
@@ -1,20 +1,30 @@
 <script lang="ts">
   import type { HeaderOption } from "@/types/types";
+  import Option from "@/components/Header/Option.svelte";
 
   export let activeNavigationOption: HeaderOption;
 
   let hovered = false;
 </script>
 
-<div class="fixed inset-0 w-screen h-screen bg-silver flex items-center">
+<div
+  class="fixed inset-0 w-screen h-screen bg-silver flex items-center box-border py-4"
+>
   <nav
-    class={`bg-black ${
+    class={`bg-black rounded-r-md ${
       hovered ? "translate-x-0" : "-translate-x-full"
-    } h-full p-8 relative transition-transform`}
+    } h-full p-8 relative transition-transform items-center justify-center flex flex-col`}
     on:mouseover={() => (hovered = true)}
     on:mouseleave={() => (hovered = false)}
     on:focus={() => {}}
   >
     <div class="absolute z-10 top-0 bottom-0 left-full h-full w-4" />
+    <Option
+      colorOnHover={"#ffffff"}
+      destination={"/"}
+      iconName={"briefcase"}
+      navigationOption={"home"}
+      text={"Home"}
+    />
   </nav>
 </div>
---
+++ src/components/Header/Option.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  import type { HeaderOption } from "@/types/types";
+
+  export let navigationOption: HeaderOption;
+  export let colorOnHover: string;
+  export let iconName: string;
+  export let text: string;
+  export let destination: string;
+</script>
+
+<a href={destination} class="hover:text-silver-100 flex gap-4 items-center">
+  <i class={`fa-solid fa-${iconName} `} />
+  <p>{text}</p>
+</a>
