767c1feae74fd6e69794f640ccecb3af7c23af58
Fatih20 <fnri39@protonmail.com>
2023-06-19 11:12:56 +0700

    Responsive at home.

--- src/components/Header/Header.svelte
+++ src/components/Header/Header.svelte
@@ -4,33 +4,42 @@
 
   export let activeNavigationOption: HeaderOption;
 
-  let hovered = false;
+  let open = false;
 </script>
 
 <div
   class={`fixed inset-0 z-20 w-screen h-screen pointer-events-none ${
-    hovered ? "bg-slate-900/75" : ""
+    open ? "bg-slate-900/75" : ""
   } flex items-center box-border py-8`}
 >
   <nav
     class={`pointer-events-auto bg-black rounded-r-md lg:rounded-r-xl ${
-      hovered ? "translate-x-0" : "-translate-x-full"
-    } h-full py-8 pl-4 pr-6 relative transition-transform items-start justify-center flex flex-col gap-4 duration-200`}
-    on:mouseover={() => (hovered = true)}
-    on:mouseleave={() => (hovered = false)}
+      open ? "translate-x-0" : "-translate-x-full"
+    } h-full py-2 pl-4 pr-6 relative transition-transform items-start justify-center flex flex-col gap-4 duration-200`}
+    on:mouseover={() => (open = true)}
+    on:mouseleave={() => (open = false)}
     on:focus={() => {}}
   >
     <div
-      class="absolute flex justify-center items-center flex-col z-20 top-0 bottom-0 left-full h-full"
+      class="absolute flex justify-end lg:justify-center items-center flex-col z-20 top-0 bottom-0 left-full h-full"
     >
-      <div class=" rounded-r-md pl-2 py-3 pr-3">
+      <button
+        class={`px-2 py-1 rounded-r-md bg-black-500 lg:bg-transparent lg:pointer-events-none ${
+          open ? "opacity-0" : ""
+        } transition-all`}
+        on:click={(e) => {
+          open = true;
+          e.stopPropagation();
+        }}
+      >
         <i
           class={`fa-solid fa-chevron-right text-xl text-silver-100 ${
-            hovered ? "rotate-180 opacity-0" : "rotate-0"
+            open ? "rotate-180 opacity-0" : "rotate-0"
           } transition-all duration-200`}
         />
-      </div>
+      </button>
     </div>
+    <div class="lg:hidden flex-grow" />
     <Option
       colorOnHover={"#8cd5ff"}
       destination={"/"}
@@ -63,5 +72,9 @@
       {activeNavigationOption}
       text={"MUNs"}
     />
+    <div class="lg:hidden flex-grow" />
+    <button class="lg:hidden" on:click={() => (open = false)}>
+      <i class="fa-solid fa-chevron-left text-xl text-silver-100 font-bold" />
+    </button>
   </nav>
 </div>
