154579a6f09ce1ded1f0fa52fb19620fb1afbaad
Fatih20 <fnri39@protonmail.com>
2023-06-26 10:13:42 +0700

    Hide gallery when there is no images.

--- src/components/Gallery.svelte
+++ src/components/Gallery.svelte
@@ -3,24 +3,48 @@
 
   export let title: string = "";
   export let images: Image[] = [];
+
+  let carouselShown = false;
 </script>
 
-<section
-  class="flex flex-col items-center justify-start w-full px-4 pt-4 gap-2 lg:gap-4"
->
-  <h2 class="text-3xl font-bold">
-    {title}
-  </h2>
-  <div
-    class="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-2 md:gap-3 xl:gap-4"
+{#if images.length > 0}
+  <section
+    class="flex flex-col items-center justify-start w-full px-4 pt-4 gap-2 lg:gap-4"
   >
-    {#each images as { alt, title, url }}
-      <img
-        src={url}
-        {alt}
-        {title}
-        class="rounded-sm md:rounded-md lg:rounded-lg w-full transition-all hover:scale-105 aspect-video object-cover"
-      />
-    {/each}
-  </div>
-</section>
+    <div
+      class={`fixed inset-0 bg-silver w-full h-full ${
+        carouselShown ? "" : "hidden"
+      }`}
+    >
+      bruh
+    </div>
+    <h2 class="text-3xl font-bold">
+      {title}
+    </h2>
+    <div
+      class="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-2 md:gap-3 xl:gap-4 point"
+    >
+      {#each images as { alt, title, url }}
+        <button
+          class="rounded-sm md:rounded-md lg:rounded-lg w-full aspect-video"
+          on:click={() => {
+            console.log("Bruh");
+            carouselShown = true;
+          }}
+        >
+          <img
+            src={url}
+            {alt}
+            {title}
+            on:click={() => {
+              console.log("Bruh");
+              carouselShown = true;
+            }}
+            on:keyup={() => (carouselShown = true)}
+            class="rounded-sm md:rounded-md lg:rounded-lg w-full transition-all hover:scale-105 aspect-video object-cover cursor-pointer"
+          />
+        </button>
+      {/each}
+    </div>
+  </section>
+{/if}
