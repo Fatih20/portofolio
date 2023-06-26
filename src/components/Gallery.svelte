77c60acd0cb5a276adfe8067345a9459e3b9340c
Fatih20 <fnri39@protonmail.com>
2023-06-26 09:22:38 +0700

    Initial work on gallery.

---
+++ src/components/Gallery.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+  import type { Image } from "@/types/cms";
+
+  export let title: string = "";
+  export let images: Image[] = [];
+</script>
+
+<section class="flex flex-col items-center justify-start w-full px-4 pt-4">
+  <h2 class="text-3xl font-bold">
+    {title}
+  </h2>
+  <div
+    class="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-2 md:gap-3 xl:gap-4"
+  >
+    {#each images as { alt, title, url }}
+      <img
+        src={url}
+        {alt}
+        {title}
+        class="rounded-sm md:rounded-md lg:rounded-lg w-full hover:scale-105"
+      />
+    {/each}
+  </div>
+</section>
