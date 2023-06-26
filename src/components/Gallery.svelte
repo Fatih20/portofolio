b6fc6e04fdcfbd81cd0dc3df3c1ae54b3652d933
Fatih20 <fnri39@protonmail.com>
2023-06-26 09:28:48 +0700

    Standardize gallery photos aspect ratio.

--- src/components/Gallery.svelte
+++ src/components/Gallery.svelte
@@ -5,7 +5,9 @@
   export let images: Image[] = [];
 </script>
 
-<section class="flex flex-col items-center justify-start w-full px-4 pt-4">
+<section
+  class="flex flex-col items-center justify-start w-full px-4 pt-4 gap-2 lg:gap-4"
+>
   <h2 class="text-3xl font-bold">
     {title}
   </h2>
@@ -17,7 +19,7 @@
         src={url}
         {alt}
         {title}
-        class="rounded-sm md:rounded-md lg:rounded-lg w-full hover:scale-105"
+        class="rounded-sm md:rounded-md lg:rounded-lg w-full transition-all hover:scale-105 aspect-video object-cover"
       />
     {/each}
   </div>
