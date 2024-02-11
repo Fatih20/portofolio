5f5f412914fde1fd5a652ee0c0cf1e2189336ecc
Fatih20 <fnri39@protonmail.com>
2024-02-11 15:27:11 +0700

    feat: A11y fix to overlay div

--- src/components/ActionButton.svelte
+++ src/components/ActionButton.svelte
@@ -18,6 +18,8 @@
 <div
   on:click={() => (shown = false)}
   on:keydown={() => {}}
+  role="button"
+  tabindex="-1"
   class={`fixed inset-0 z-20 w-screen h-screen ${
     shown
       ? "bg-slate-900/75 pointer-events-auto backdrop-blur-sm"
